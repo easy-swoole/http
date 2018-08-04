81fcc460ddb802186bf69c05d0a2a3319c1cee53
yf <291323003@qq.com>
2018-08-05 00:43:59 +0800

    update

--- src/AbstractInterface/Controller.php
+++ src/AbstractInterface/Controller.php
@@ -58,7 +58,7 @@ abstract class Controller
         }
     }
 
-    protected function actionNotFound($action):void
+    protected function actionNotFound(?string $action):void
     {
         $this->response()->withStatus(Status::CODE_NOT_FOUND);
     }
