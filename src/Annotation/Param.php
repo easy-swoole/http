56540adfa136c9007a2d0f9dc34bc479174ff01d
yf <291323003@qq.com>
2019-07-20 23:48:58 +0800

    up

--- src/Annotation/Param.php
+++ src/Annotation/Param.php
@@ -33,15 +33,164 @@ final class Param implements AnnotationTagInterface
 
     public $validateRuleList = [];
 
-    /**
-     * @var string
-     */
-    public $required;
+    private $allowValidateRule = [
+        'activeUrl', 'alpha', 'alphaNum', 'alphaDash', 'between', 'bool',
+        'decimal', 'dateBefore', 'dateAfter', 'equal', 'different', 'equalWithColumn',
+        'differentWithColumn', 'float', 'func', 'inArray', 'integer', 'isIp',
+        'notEmpty', 'numeric', 'notInArray', 'length', 'lengthMax', 'lengthMin',
+        'betweenLen', 'money', 'max', 'min', 'regex', 'allDigital',
+        'required', 'timestamp', 'timestampBeforeDate', 'timestampAfterDate',
+        'timestampBefore', 'timestampAfter', 'url'
+    ];
 
     /**
      * @var string
      */
+    public $activeUrl;
+    /**
+     * @var string
+     */
+    public $alpha;
+    /**
+     * @var string
+     */
+    public $alphaNum;
+    /**
+     * @var string
+     */
+    public $alphaDash;
+    /**
+     * @var string
+     */
+    public $between;
+    /**
+     * @var string
+     */
+    public $bool;
+    /**
+     * @var string
+     */
+    public $decimal;
+    /**
+     * @var string
+     */
+    public $dateBefore;
+    /**
+     * @var string
+     */
+    public $dateAfter;
+    /**
+     * @var string
+     */
+    public $equal;
+    /**
+     * @var string
+     */
+    public $different;
+    /**
+     * @var string
+     */
+    public $equalWithColumn;
+    /**
+     * @var string
+     */
+    public $differentWithColumn;
+    /**
+     * @var string
+     */
+    public $float;
+    /**
+     * @var string
+     */
+    public $func;
+    /**
+     * @var string
+     */
+    public $inArray;
+    /**
+     * @var string
+     */
+    public $integer;
+    /**
+     * @var string
+     */
+    public $isIp;
+    /**
+     * @var string
+     */
+    public $notEmpty;
+    /**
+     * @var string
+     */
+    public $numeric;
+    /**
+     * @var string
+     */
+    public $notInArray;
+    /**
+     * @var string
+     */
+    public $length;
+    /**
+     * @var string
+     */
     public $lengthMax;
+    /**
+     * @var string
+     */
+    public $lengthMin;
+    /**
+     * @var string
+     */
+    public $betweenLen;
+    /**
+     * @var string
+     */
+    public $money;
+    /**
+     * @var string
+     */
+    public $max;
+    /**
+     * @var string
+     */
+    public $min;
+    /**
+     * @var string
+     */
+    public $regex;
+    /**
+     * @var string
+     */
+    public $allDigital;
+    /**
+     * @var string
+     */
+    public $required;
+    /**
+     * @var string
+     */
+    public $timestamp;
+    /**
+     * @var string
+     */
+    public $timestampBeforeDate;
+    /**
+     * @var string
+     */
+    public $timestampAfterDate;
+    /**
+     * @var string
+     */
+    public $timestampBefore;
+    /**
+     * @var string
+     */
+    public $timestampAfter;
+    /**
+     * @var string
+     */
+    public $url;
 
     public function tagName(): string
     {
@@ -70,7 +219,7 @@ final class Param implements AnnotationTagInterface
                 $this->alias = trim($args['alias']," \t\n\r\0\x0B\"'");
             }else{
                 $key = array_keys($args)[0];
-                if(property_exists($this,$key)){
+                if(in_array($key,$this->allowValidateRule)){
                     $value = trim($args[$key]," \t\n\r\0\x0B\"'");
                     $temp = explode("|",$value);
                     $list = [];
