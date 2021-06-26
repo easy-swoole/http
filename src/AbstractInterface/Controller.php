746b739b80dcb2ddcb62ed394c6abb8c3662dc3f
如果的如果 <admin@fosuss.com>
2021-06-26 21:24:29 +0800

    兼容php8

--- src/AbstractInterface/Controller.php
+++ src/AbstractInterface/Controller.php
@@ -138,8 +138,9 @@ abstract class Controller
 
     protected function xml($options = LIBXML_NOERROR | LIBXML_NOCDATA, string $className = 'SimpleXMLElement')
     {
-        //禁止引用外部xml实体
-        libxml_disable_entity_loader(true);
+        if (\PHP_VERSION_ID < 80000 || \LIBXML_VERSION < 20900){
+            libxml_disable_entity_loader(true);
+        }
         return simplexml_load_string($this->request()->getBody()->__toString(), $className, $options);
     }
 
