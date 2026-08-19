0cbe84db9082bfe677d4e3d0cb72bc963e83e6c7
如果的如果 <admin@fosuss.com>
2026-08-19 21:36:06 +0800

    新增HttpContext

--- src/Context.php
+++ src/Context.php
@@ -3,16 +3,36 @@
 namespace EasySwoole\Http;
 
 use EasySwoole\Component\Singleton;
+use EasySwoole\Http\Exception\Exception;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use Swoole\Coroutine;
 
 class Context
 {
     use Singleton;
 
+    const KEY_HTTP_REQUEST = 'KEY_HTTP_REQUEST';
+
+    const KEY_HTTP_RESPONSE = 'KEY_HTTP_RESPONSE';
+
     protected array $context = [];
 
     function set(string $key, $value):void
     {
+        if($key == self::KEY_HTTP_REQUEST)
+        {
+            if(!$value instanceof ServerRequestInterface){
+                throw new Exception('KEY_HTTP_REQUEST value must be ServerRequestInterface');
+            }
+        }
+        if($key == self::KEY_HTTP_RESPONSE){
+            if(!$value instanceof ResponseInterface){
+                throw new Exception('KEY_HTTP_RESPONSE value must be ResponseInterface');
+            }
+        }
+
+
         $cId = Coroutine::getCid();
         if(!isset($this->context[$cId])){
             Coroutine::defer(function () use ($cId){
